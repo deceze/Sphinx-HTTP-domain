170fcfff050c30974776599b96de4d70f94bcfa3
Simon Law <sfllaw@sfllaw.ca>
2011-03-10 22:07:21 -0500

    Pretty output for HTTP method signatures.

--- sphinx_http_domain/__init__.py
+++ sphinx_http_domain/__init__.py
@@ -9,14 +9,20 @@
     :license: BSD, see LICENSE for details
 """
 
+from itertools import izip
 import re
+from urlparse import urlsplit, parse_qsl
 
-from docutils import nodes
+from docutils.nodes import Text
 
-from sphinx import addnodes
 from sphinx.locale import l_
 from sphinx.domains import Domain, ObjType
 from sphinx.directives import ObjectDescription
+
+from sphinx_http_domain.nodes import (desc_http_method, desc_http_url,
+                                      desc_http_path, desc_http_patharg,
+                                      desc_http_query, desc_http_queryparam,
+                                      desc_http_fragment)
 
 
 class HTTPMethod(ObjectDescription):
@@ -25,35 +31,76 @@ class HTTPMethod(ObjectDescription):
     """
     # RE for HTTP method signatures
     sig_re = re.compile(
-        r'^'
-        r'(GET|POST|PUT|DELETE)?\s?'    # verb
-        r'(\S+)'                        # url
-        r'(.*)'                         # query
-        r'$'
+        (
+            r'^'
+            r'(?:(GET|POST|PUT|DELETE)\s+)?'  # HTTP method
+            r'(.+)'                           # URL
+            r'\s*$'
+        ),
+        re.IGNORECASE
     )
+
+    # Note, path_re.findall() will produce an extra ('', '') tuple
+    # at the end of its matches. You should strip it off, or you will
+    path_re = re.compile(
+        (
+            r'([^{]*)'                  # Plain text
+            r'(\{[^}]*\})?'             # {arg} in matched braces
+        ),
+        re.VERBOSE
+    )
+
+    def handle_method(self, method, signode):
+        if method is None:
+            method = 'GET'
+        method = method.upper()
+        signode += desc_http_method(method, method)
+
+    def handle_url(self, url, signode):
+        if url is None:
+            raise ValueError
+        # Split URL into path, query, and fragment
+        _, _, path, query, fragment = urlsplit(url)
+        urlnode = desc_http_url()
+        # Create nodes for the path
+        if path:
+            pathnode = desc_http_path(path)
+            path_segments = self.path_re.findall(path)[:-1]
+            for text, arg in path_segments:
+                pathnode += Text(text)
+                if arg:
+                    arg = arg[1:-1]     # Strip off { and }
+                    pathnode += desc_http_patharg(arg, arg)
+            urlnode += pathnode
+        else:
+            raise ValueError
+        # Create nodes for the query string
+        if query:
+            querynode = desc_http_query(query)
+            query_params = query.split('&')
+            for p in query_params:
+                querynode += desc_http_queryparam(p, p)
+            urlnode += querynode
+        # Create a node for the fragment
+        if fragment:
+            urlnode += desc_http_fragment(fragment, fragment)
+        # Add urlnode to signode
+        signode += urlnode
 
     def handle_signature(self, sig, signode):
         """
         Transform an HTTP method signature into RST nodes.
-        Returns (method name, classname if any).
+        Returns (method name, full URL).
         """
-        m = sig_re.match(sig)
+        # Match the signature to extract the method and URL
+        m = self.sig_re.match(sig)
         if m is None:
             raise ValueError
-
-        verb, url, query = m.groups()
-        if verb is None:
-            verb = 'GET'
-
-        signode += addnodes.desc_addname(verb, verb)
-        signode += addnodes.desc_name(url, url)
-
-        if query:
-            params = query.strip().split()
-            for param in params:
-                signode += addnodes.desc_optional(param, param)
-
-        return url
+        method, url = m.groups()
+        # Append nodes to signode for method and url
+        self.handle_method(method, signode)
+        self.handle_url(url, signode)
+        return (method, url)
 
 
 class HTTPDomain(Domain):
@@ -73,3 +120,10 @@ class HTTPDomain(Domain):
 
 def setup(app):
     app.add_domain(HTTPDomain)
+    desc_http_method.contribute_to_app(app)
+    desc_http_url.contribute_to_app(app)
+    desc_http_path.contribute_to_app(app)
+    desc_http_patharg.contribute_to_app(app)
+    desc_http_query.contribute_to_app(app)
+    desc_http_queryparam.contribute_to_app(app)
+    desc_http_fragment.contribute_to_app(app)
---
+++ sphinx_http_domain/nodes.py
@@ -0,0 +1,299 @@
+# -*- coding: utf-8 -*-
+"""
+    Nodes for the HTTP domain.
+"""
+
+from docutils import nodes
+
+from sphinx.util.texescape import tex_escape_map
+
+
+class HttpNode(nodes.Part, nodes.Inline, nodes.TextElement):
+    """Generic HTTP node."""
+    _writers = ['text', 'html', 'latex', 'man']
+
+    def set_first(self):
+        try:
+            self.children[0].first = True
+        except IndexError:
+            pass
+
+    @classmethod
+    def contribute_to_app(cls, app):
+        kwargs = {}
+        for writer in cls._writers:
+            visit = getattr(cls, 'visit_' + writer, None)
+            depart = getattr(cls, 'depart_' + writer, None)
+            if visit and depart:
+                kwargs[writer] = (visit, depart)
+        app.add_node(cls, **kwargs)
+
+    @staticmethod
+    def visit_text(self, node):
+        pass
+
+    @staticmethod
+    def depart_text(self, node):
+        pass
+
+    @staticmethod
+    def visit_latex(self, node):
+        pass
+
+    @staticmethod
+    def depart_latex(self, node):
+        pass
+
+    @staticmethod
+    def visit_man(self, node):
+        pass
+
+    @staticmethod
+    def depart_man(self, node):
+        pass
+
+
+class desc_http_method(HttpNode):
+    """HTTP method node."""
+    def astext(self):
+        return nodes.TextElement.astext(self) + ' '
+
+    @staticmethod
+    def depart_text(self, node):
+        self.add_text(' ')
+
+    @staticmethod
+    def visit_html(self, node):
+        self.body.append(self.starttag(node, 'tt', '',
+                                       CLASS='descclassname deschttpmethod'))
+
+    @staticmethod
+    def depart_html(self, node):
+        self.body.append(' </tt>')
+
+    @staticmethod
+    def visit_latex(self, node):
+        self.body.append(r'\code{')
+        self.literal_whitespace += 1
+
+    @staticmethod
+    def depart_latex(self, node):
+        self.body.append(r'}~')
+        self.literal_whitespace -= 1
+
+    @staticmethod
+    def depart_man(self, node):
+        self.body.append(r'\~')
+
+
+class desc_http_url(HttpNode):
+    """HTTP URL node."""
+    @staticmethod
+    def visit_html(self, node):
+        self.body.append(self.starttag(node, 'tt', '',
+                                       CLASS='descname deschttpurl'))
+
+    @staticmethod
+    def depart_html(self, node):
+        self.body.append('</tt>')
+
+    @staticmethod
+    def visit_latex(self, node):
+        self.body.append(r'\bfcode{')
+        self.literal_whitespace += 1
+
+    @staticmethod
+    def depart_latex(self, node):
+        self.body.append(r'}')
+        self.literal_whitespace -= 1
+
+
+class desc_http_path(HttpNode):
+    """HTTP path node. Contained in the URL node."""
+    @staticmethod
+    def visit_html(self, node):
+        self.body.append(self.starttag(node, 'span', '',
+                                       CLASS='deschttppath'))
+
+    @staticmethod
+    def depart_html(self, node):
+        self.body.append('</span>')
+
+
+class desc_http_patharg(HttpNode):
+    """
+    HTTP path argument node. Contained in the path node.
+
+    This node is created when {argument} is found inside the path.
+    """
+    wrapper = (u'{', u'}')
+
+    def astext(self, node):
+        return (self.wrapper[0] +
+                nodes.TextElement.astext(node) +
+                self.wrapper[1])
+
+    @staticmethod
+    def visit_text(self, node):
+        self.add_text(node.wrapper[0])
+
+    @staticmethod
+    def depart_text(self, node):
+        self.add_text(node.wrapper[1])
+
+    @staticmethod
+    def visit_html(self, node):
+        self.body.append(
+            self.starttag(node, 'em', '', CLASS='deschttppatharg') +
+            self.encode(node.wrapper[0]) +
+            self.starttag(node, 'span', '', CLASS='deschttppatharg')
+        )
+
+    @staticmethod
+    def depart_html(self, node):
+        self.body.append('</span>' +
+                         self.encode(node.wrapper[1]) +
+                         '</em>')
+
+    @staticmethod
+    def visit_latex(self, node):
+        self.body.append('\emph{' +
+                         node.wrapper[0].translate(tex_escape_map))
+
+    @staticmethod
+    def depart_latex(self, node):
+        self.body.append(node.wrapper[1].translate(tex_escape_map) +
+                         '}')
+
+    @staticmethod
+    def visit_man(self, node):
+        self.body.append(self.defs['emphasis'][0])
+        self.body.append(self.deunicode(node.wrapper[0]))
+
+    @staticmethod
+    def depart_man(self, node):
+        self.body.append(self.deunicode(node.wrapper[1]))
+        self.body.append(self.defs['emphasis'][1])
+
+
+class desc_http_query(HttpNode):
+    """HTTP query string node. Contained in the URL node."""
+    prefix = u'?'
+
+    def astext(self):
+        return self.prefix + nodes.TextElement.astext(self)
+
+    @staticmethod
+    def visit_text(self, node):
+        self.add_text(node.prefix)
+        node.set_first()
+
+    @staticmethod
+    def visit_html(self, node):
+        self.body.append(
+            self.starttag(node, 'span', '', CLASS='deschttpquery') +
+            self.encode(node.prefix)
+        )
+        node.set_first()
+
+    @staticmethod
+    def depart_html(self, node):
+        self.body.append('</span>')
+
+    @staticmethod
+    def visit_latex(self, node):
+        self.body.append(node.prefix.translate(tex_escape_map))
+        node.set_first()
+
+    @staticmethod
+    def visit_man(self, node):
+        self.body.append(self.deunicode(node.prefix))
+        node.set_first()
+
+
+class desc_http_queryparam(HttpNode):
+    """
+    HTTP query string parameter node. Contained in the query string node.
+
+    This node is created for each parameter inside a query string.
+    """
+    child_text_separator = u'&'
+    first = False
+
+    @staticmethod
+    def visit_text(self, node):
+        if not node.first:
+            self.add_text(node.child_text_separator)
+
+    @staticmethod
+    def visit_html(self, node):
+        if not node.first:
+            self.body.append(self.encode(node.child_text_separator))
+        self.body.append(self.starttag(node, 'em', '',
+                                       CLASS='deschttpqueryparam'))
+
+    @staticmethod
+    def depart_html(self, node):
+        self.body.append('</em>')
+
+    @staticmethod
+    def visit_latex(self, node):
+        if not node.first:
+            self.body.append(
+                node.child_text_separator.translate(tex_escape_map)
+            )
+        self.body.append('\emph{')
+
+    @staticmethod
+    def depart_latex(self, node):
+        self.body.append('}')
+
+    @staticmethod
+    def visit_man(self, node):
+        if not node.first:
+            self.body.append(self.deunicode(node.child_text_separator))
+        self.body.append(self.defs['emphasis'][0])
+
+    @staticmethod
+    def depart_man(self, node):
+        self.body.append(self.defs['emphasis'][1])
+
+
+class desc_http_fragment(HttpNode):
+    """HTTP fragment node. Contained in the URL node."""
+    prefix = u'#'
+
+    def astext(self):
+        return self.prefix + nodes.TextElement.astext(self)
+
+    @staticmethod
+    def visit_text(self, node):
+        self.add_text(node.prefix)
+
+    @staticmethod
+    def visit_html(self, node):
+        self.body.append(self.encode(node.prefix) +
+                         self.starttag(node, 'em', '',
+                                       CLASS='deschttpfragment'))
+
+    @staticmethod
+    def depart_html(self, node):
+        self.body.append('</em>')
+
+    @staticmethod
+    def visit_latex(self, node):
+        self.body.append(node.prefix.translate(tex_escape_map) +
+                         '\emph{')
+
+    @staticmethod
+    def depart_latex(self, node):
+        self.body.append('}')
+
+    @staticmethod
+    def visit_man(self, node):
+        self.body.append(self.deunicode(node.prefix))
+        self.body.append(self.defs['emphasis'][0])
+
+    @staticmethod
+    def depart_man(self, node):
+        self.body.append(self.defs['emphasis'][1])
