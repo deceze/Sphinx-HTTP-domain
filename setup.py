a94f8a6da427226b9b2061584384cb302d61d2d9
Simon Law <sfllaw@sfllaw.ca>
2011-03-11 11:38:05 -0500

    Add long_description to setup, based on README.rst.

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
+import os
+
 from distutils.core import setup
+
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
 
 setup(
     name='sphinx-http-domain',
     version='0.1',
     description='Sphinx domain to mark up RESTful web services in ReST',
+    long_description=long_description,
     url='https://github.com/deceze/Sphinx-HTTP-domain/',
     author='David Zentgraf',
     author_email='deceze@gmail.com',
